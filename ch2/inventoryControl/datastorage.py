d82f328e22cb1d17bcc8fa0f98c6d670b5a0845c
theevilaunt <theevilaunt@2akitas.net>
2016-12-27 15:49:30 -0800

    first commit

---
+++ ch2/inventoryControl/datastorage.py
@@ -0,0 +1,23 @@
+def init():
+	_load_items()
+
+def items():
+	pass
+
+def products():
+	pass
+
+def locations():
+	pass
+
+def add_item(product_code, location_code):
+	pass
+
+def remove_item(prodcut_code, location_code):
+	pass
+
+def set_products(products):
+	pass
+
+def set_locations(locations):
+	pass
